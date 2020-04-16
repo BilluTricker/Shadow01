f93de463667b994cf795a2bffe9f1e7c19c2a70a
BilluTricker <63787819+BilluTricker@users.noreply.github.com>
2020-04-17 01:36:31 +0500

    Update and rename zeroboy.py to shadow01.py

--- shadow01.py
+++ shadow01.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•BILLU - TRICKER -OFFICAL------•◈•
+|__\~~) •||•ITS - SHADOW---------------•◈•
+|__(-----\  •◈•------TEAM-DARK--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBillu Tricker
+\033[1;96mInstagram \033[1;93m: \033[1;FollowShadow
+\033[1;96mFacebook  \033[1;93m: \033[1; Shadow077
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/BilluTricker/Shadow01
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBillu Tricker
+\033[1;96mInstagram \033[1;93m: \033[1;92mFollowShadow
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Shadow077
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/BilluTricker/Shadow01
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Shadow"
+CorrectPassword = "Shadow"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.youtube.com/channel/UCzEDJq-N-CjDzepos5CV0_g')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.youtube.com/channel/UCzEDJq-N-CjDzepos5CV0_g')
 
 def login():
 	os.system('clear')
